fbe59b9270dbcfd4ef3edb04c97e319481cd7782
MyeongWon Son <52558212+smw1994@users.noreply.github.com>
2022-11-09 21:45:52 +0900

    Add files via upload

---
+++ #DFS (Depth First Search).py
@@ -0,0 +1,10 @@
+#DFS (Depth First Search)
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
